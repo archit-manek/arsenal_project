b6da3d2e1b3a0e11c0eed53ff64f2cb4b75a4ab6
Archit Manek <archm@architmanek.local>
2023-09-17 14:23:17 -0600

    fixed config issue

--- api/fetch.py
+++ api/fetch.py
@@ -3,9 +3,7 @@ from settings import properties
 from settings import endpoints
 
 def get_goals_scored():
-    response = requests.get(f'{endpoints.BASE_URL}/team?key={properties.API_KEY}')
+    response = requests.get(f'{endpoints.BASE_URL}/team?key={properties.TRIAL_KEY}')
     if response.status_code == 200:
         return response.json()
     return None
-
-print(get_goals_scored())
---
+++ main.py
@@ -0,0 +1,7 @@
+from api.fetch import get_goals_scored
+
+def main():
+    print(get_goals_scored())
+
+if __name__ == "__main__":
+    main()
